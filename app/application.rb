26fa95bb6df657a3460d41e08ca2b874929a9996
Barbara Johnson <johnson.barbara@gmail.com>
2019-05-18 16:32:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,10 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
+      
+      #song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      #song = @@songs.find{|s| s.title == song_title}
+      
       @@items.each do |item|
         resp.write "#{item}\n"
       end
